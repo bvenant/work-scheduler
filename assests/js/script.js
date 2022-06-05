b94cb3c693e9df2a9a18aefda28473bc008e57a6
Bradley V <bvenant@aol.com>
2022-06-05 10:21:26 -0400

    adding time block section to js

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,10 +1,13 @@
+var saveBtn = $(".saveBtn");
+
+
 $(document).ready(function() {
 
 // Current day displayed in calendar
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
 // Saves when button is clicked
-$(saveBtn).on("click", function() {
+saveBtn.on("click", function() {
     var plan = $(this).siblings(".planner").val();
     var clock = $(this).parent().attr("id");
 
@@ -12,30 +15,45 @@ $(saveBtn).on("click", function() {
     localStorage.setItem(plan, clock);
 });
 
+// Color coded to verify past, present, or future
 var colorCode = function() {
-    
-}
+    var currentHour = moment().hour();
 
+    $(".time-block").each(function() {
+        var hours = parseInt($(this).attr("id"));
+
+        // console.log(this);
+
+        if (hours > currentHour) {
+            $(this).addClass("future");
+        } else if (hours === currentHour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    });
+};
+colorCode();
 });
 
 
-$("#9").addClass("past");
-$("#6").addClass("past");
-console.log(moment().hour()-12);
-// =========
-var currentHour = moment().hour();
+// $("#9").addClass("past");
+// $("#6").addClass("past");
+// console.log(moment().hour()-12);
+// // =========
+// var currentHour = moment().hour();
   
-if (currentHour > 12) {
-    currentHour = currentHour - 12
-}
-if (currentHour === 5) {
-    $("#6").addClass("present");
-} 
-// =========
+// if (currentHour > 12) {
+//     currentHour = currentHour - 12
+// }
+// if (currentHour === 5) {
+//     $("#6").addClass("present");
+// } 
+// // =========
 
-for (var i; i < currentHour.length; i++) {
+// for (var i; i < currentHour.length; i++) {
 
-}
+// }
 
 // iterate over hours array
 //  if current hour is less than element
